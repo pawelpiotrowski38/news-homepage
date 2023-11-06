9a04cb5aae0f475021f6c92f480cb476ef1674c6
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2023-11-06 20:15:05 +0100

    Change header element number (from h2 to h3) of list elements in new articles section

--- src/components/NewArticlesSection.jsx
+++ src/components/NewArticlesSection.jsx
@@ -9,9 +9,9 @@ export default function NewArticlesSection({ articles }) {
             <ul className="new-articles-section__list">
                 {articles.map((article, index) => (
                     <li key={article.id} className="new-articles-section__list-item">
-                        <h2 className="new-articles-section__list-item-title">
+                        <h3 className="new-articles-section__list-item-title">
                             {article.title}
-                        </h2>
+                        </h3>
                         <p className="new-articles-section__list-item-desc">
                             {article.desc}
                         </p>
